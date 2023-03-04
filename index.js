1f27dac58488ac4f5f0394482b55a837e1698cc7
Ogo43 <emehaogochukwu@gmail.com>
2023-03-04 21:09:09 +0100

    Assignment 3

---
+++ index.js
@@ -0,0 +1,66 @@
+// A1
+
+// Mutating array methods are methods that change the object after the method has been used. 
+// Examples: prototype.pop(), prototype.push(), prototype.shift(), prototype.unshift(), prototype.splice()
+
+// Non-mutating array methods are methods that do not change the object after the method has been used.
+// Examples: prototype.slice(), prototype.concat(), prototype.map(), prototype.filter(), prototype.forEach()
+
+// A2
+
+const languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python'];
+
+const kotlin = languages.push('Kotlin');
+console.log(languages);
+// [ 'C#', 'JavaScript', 'Ruby', 'PHP', 'Python', 'Kotlin' ]
+const java = languages.splice(3, 0, 'Java');
+console.log(languages);
+// [ 'C#', 'JavaScript', 'Ruby', 'Java', 'PHP', 'Python' ]
+const removeFirstItem = languages.shift('C#');
+console.log(languages);
+// [ 'JavaScript', 'Ruby', 'PHP', 'Python' ]
+const addScalaSwift = languages.unshift('Scala', 'Swift');
+console.log(languages);
+// [ 'Scala', 'Swift', 'C#', 'JavaScript', 'Ruby', 'PHP', 'Python' ]
+const replacePHP = languages.splice(3, 1, 'Go', 'Rust');
+console.log(languages);
+// [ 'C#', 'JavaScript', 'Ruby', 'Go', 'Rust', 'Python' ]
+
+// A3
+
+let fruit = ['apple', 'mango', 'banana'];
+
+function changeFruit (fruit) {
+    fruit[2] = "orange";
+    return fruit;
+}
+
+console.log(fruit.push("orange")); //The value of fruit is 4
+
+// A4
+
+function max_Number (numbers) {
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+const rangeOfNumber = [4,5,10,-2];
+console.log(max_Number(rangeOfNumber)); // the max is 10
+
+// A5
+
+const valTimesIndex = (numbers) => {
+    let multi = [];
+    for (let i = 0; i < numbers.length; i++) {
+        multi.push((numbers[i] * i));
+    }
+    return multi;
+}
+
+const indexMultiples = [1, 2, 3];
+console.log(valTimesIndex(indexMultiples));  //[ 0, 2, 6 ]
